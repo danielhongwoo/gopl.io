b2ddd57dbc26a49907b11fd75642e36886072476
Daniel Lee <daniel.hongwoo.lee@gmail.com>
2019-12-08 14:45:33 +0900

    ch1

---
+++ ch1/exercise1.2/main.go
@@ -0,0 +1,16 @@
+// Modify the echo program to print the index and value of each of its arguments, on per line
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//!+
+func main() {
+	for index, arg := range os.Args[1:] {
+    fmt.Println(index, arg)
+	}
+}
+
+//!-
---
+++ ch1/exercise1.3/main.go
@@ -0,0 +1,26 @@
+// Experiment to measure the difference in running time between our potentially inefficient versions and the one that uses strings.Join. (Section 1.6 illustrates part of the time package, and Section 11.4 shows how to write benchmark tests for systematic performance evalation.)
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+  "time"
+)
+
+func main() {
+  start1 := time.Now()
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+  fmt.Println(time.Since(start1).Seconds())
+
+  start2 := time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+  fmt.Println(time.Since(start2).Seconds())
+}
+
+//!-
